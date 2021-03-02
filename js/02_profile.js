b06978d5a2e0a2871834e310a71eeae52a076015
j-hyuny <jihyun6651@naver.com>
2021-03-02 23:34:49 +0900

    my_site

--- js/02_profile.js
+++ js/02_profile.js
@@ -1,7 +1,12 @@
 $(window).ready(function () {
   draw(100, '.pie-chart1', '#3CACFF')
   draw(95, '.pie-chart2', '#FF9F0D')
+  draw(95, '.pie-chart3', '#3CACFF')
+  draw(95, '.pie-chart4', '#F24E1E')
+  draw(70, '.pie-chart5', '#e65127')
+  draw(70, '.pie-chart6', '#0c73b8')
   draw(70, '.pie-chart7', '#E7A328')
+  draw(70, '.pie-chart8', '#0769ad')
 })
 
 function draw(max, classname, colorname) {
@@ -24,5 +29,10 @@ function color1(i, classname, colorname) {
 function replay() {
   draw(100, '.pie-chart1', '#3CACFF')
   draw(95, '.pie-chart2', '#FF9F0D')
+  draw(95, '.pie-chart3', '#3CACFF')
+  draw(95, '.pie-chart4', '#F24E1E')
+  draw(70, '.pie-chart5', '#e65127')
+  draw(70, '.pie-chart6', '#0c73b8')
   draw(70, '.pie-chart7', '#E7A328')
+  draw(70, '.pie-chart8', '#0769ad')
 }
